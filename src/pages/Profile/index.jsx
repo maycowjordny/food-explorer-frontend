f2569f98c45395aae320c72f787962ccf2b13859
Maycow jordny <maycowjordny2015@gmail.com>
2023-07-07 23:39:09 -0300

    atualizar perfil do usuário

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -6,19 +6,40 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { useState } from "react";
 import { useAuth } from "../../hooks/auth";
-import { AvatarPlaceHolder } from "../../assets/avatarPlaceHolder.jpg"
+import ProfileAvatarPlaceholder from "../../assets/ProfileAvatarPlaceholder.png"
+import { Footer } from "../../componentes/footer"
+import { api } from "../../service/api";
 
 export function Profile() {
     const { user, updateProfile } = useAuth()
+
     const [name, setName] = useState(user.name)
     const [email, setEmail] = useState(user.email)
+    const [passwordOld, setPasswordOld] = useState()
+    const [newPassword, setNewpassord] = useState()
 
-    const [password, setPassword] = useState("")
-    const [newPassword, setNewpassord] = useState("")
-
-    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : AvatarPlaceHolder;
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : ProfileAvatarPlaceholder;
     const [avatar, setAvatar] = useState(avatarUrl)
     const [avatarFile, setAvatarFile] = useState(null)
+
+    async function handleUpdate() {
+        const user = {
+            name,
+            email,
+            password: newPassword,
+            old_password: passwordOld,
+        }
+        await updateProfile({ user, avatarFile })
+    }
+    console.log(user)
+
+    function handleChangeAvatar(event) {
+        const file = event.target.files[0]
+        setAvatarFile(file)
+
+        const imagePreview = URL.createObjectURL(file)
+        setAvatar(imagePreview)
+    }
 
     return (
         <Container>
@@ -28,37 +49,51 @@ export function Profile() {
             <Form>
 
                 <ProfileImg>
-                    <img src="https://github.com/maycowjordny.png" alt="Foto do usuário" />
+                    <img src={avatar}
+                        alt="Foto do usuário"
+                    />
                     <label htmlFor="avatar">
-
                         <FiCamera />
-                        <input id="avatar" type="file" />
+                        <input
+                            id="avatar"
+                            type="file"
+                            onChange={handleChangeAvatar}
+                        />
                     </label>
                 </ProfileImg>
 
                 <Input
                     icon={FiUser}
-                    type="name"
+                    type="text"
+                    value={name}
                     placeholder="Nome"
+                    onChange={e => setName(e.target.value)}
                 />
                 <Input
                     icon={FiMail}
                     type="email"
+                    value={email}
                     placeholder="E-mail"
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <Input
                     icon={FiLock}
                     type="password"
                     placeholder="Senha atual"
+                    onChange={e => setPasswordOld(e.target.value)}
                 />
                 <Input
                     icon={FiLock}
                     type="password"
                     placeholder="Nova senha"
+                    onChange={e => setNewpassord(e.target.value)}
                 />
 
-                <Button title="Salvar" />
+                <Button title="Salvar"
+                    onClick={handleUpdate}
+                />
             </Form>
+            <Footer />
         </Container>
     )
 }
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
+    width: 100%;
+    min-height: 100vh;
+    position: relative;
+    padding:0 0 15rem;
+
 header{
     display: flex;
     align-items: center;
